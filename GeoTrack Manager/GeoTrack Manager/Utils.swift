547fdf16b565d8ce7499e22c78ab1f0bfc49ce49
wangyiwei <wangyw.dev@outlook.com>
2023-10-24 16:55:46 +0800

    init pods

---
+++ GeoTrack Manager/GeoTrack Manager/Utils.swift
@@ -0,0 +1,11 @@
+//
+//  Utils.swift
+//  GeoTrack Manager
+//
+//  Created by leo on 2023-10-24.
+//  Copyright © 2023 wyw. All rights reserved.
+//
+
+import Foundation
+
+let amapAPIKey = "a3e55bf9145e1e395ee4a1c31b5e668c"
---
+++ GeoTrack Manager/Notes.md
@@ -0,0 +1,11 @@
+tutorials
+
+
+
+https://lbs.amap.com/api/lightweight-ios-sdk/guide/create-map/show-map
+
+
+
+find
+
+mapbox alternative free
